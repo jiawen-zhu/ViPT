3c98f202cf96044194213383c86fadf6d76114fb
jiawen-zhu <jiawen@mail.dlut.edu.cn>
2023-03-20 22:22:08 +0800

    fix bugs

--- eval_rgbd.sh
+++ eval_rgbd.sh
@@ -5,7 +5,7 @@ vot analysis --nocache --name vipt_deep
 cd ..
 
 # eval vot22-rgbd
-cd vot22_RGBD_workspace
+cd VOT22RGBD_workspace
 vot evaluate --workspace ./ vipt_deep
 vot analysis --nocache --name vipt_deep
 cd ..
